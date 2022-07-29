8a357cc8263d2cae2731a16c31feaeca91661917
Adriel Lira <adrieldiasdossantoslira@hotmail.com>
2022-07-29 15:51:01 -0300

    Início - com variáveis

---
+++ variaveis/script.js
@@ -0,0 +1,15 @@
+console.log("Oi, isso mesmo!");
+
+var nome = "André"; // Es
+let idade = 28;
+const possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+//Evitem repetições
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
