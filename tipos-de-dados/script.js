f5a0676e081737d1c3604573c2887960c25ac111
Adriel Lira <adrieldiasdossantoslira@hotmail.com>
2022-08-08 14:26:03 -0300

    Criação de arquivo tipo de dados

---
+++ tipos-de-dados/script.js
@@ -0,0 +1,12 @@
+// Tipos de dados 
+
+// Todos primitivos exceto os objetos
+var nome = 'André'; // String
+var idade = 28; // Number
+var possuiFaculdade = true; // Boolean
+var time; // Undefined
+var comida = null // Null
+var simbolo = symbol() // symbol
+var novoObjeto = {} // obeject
+
+// Primitivos são dados imutáveis
