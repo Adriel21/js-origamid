772b74818b884bb99155543398f6bb8bb1af6107
Adriel Lira <adrieldiasdossantoslira@hotmail.com>
2022-08-08 14:10:57 -0300

    Finalizado aula de variáveis

--- variaveis/script.js
+++ variaveis/script.js
@@ -1,8 +1,8 @@
 console.log("Oi, isso mesmo!");
 
-var nome = "André"; // Es
-let idade = 28;
-const possuiFaculdade = true;
+// var nome = "André"; // Es
+// let idade = 28;
+// const possuiFaculdade = true;
 
 console.log(nome, idade, possuiFaculdade);
 
@@ -13,3 +13,40 @@ var totalComprado = 5;
 var totalPreco = totalComprado * preco;
 
 console.log(totalPreco);
+
+var sobrenome = 'Rafael', 
+cidade = 'Rio';
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+
+console.log(semDefinir);
+
+// console.log(seDefinir);
+
+// var time = "Vasco";
+// time = 'Flamengo';
+
+// console.log(time);
+
+// Exercício JavaScript
+
+//Declarar uma variável com o seu nome
+
+var nome = 'Adriel';
+
+//Declarar uma variável com a sua idade
+var idade = 21;
+
+// Declarar uma variável com a sua comida favorita e não atribuir valor
+
+var $comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+
+$comidaFavorita = 'lasanha';
+
+// Declarar 5 variáveis diferentes sem valores
+
+var php, python, java, flutter, kotlin;
